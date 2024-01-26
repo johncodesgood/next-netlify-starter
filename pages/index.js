8668703de946ca51a4be703e2171e87e22676237
johncodesgood <john@fairclaims.com>
2024-01-26 12:36:49 -0800

    initial release

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,77 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
 
 export default function Home() {
+  function handleClick(e) {
+    e.preventDefault;
+    const inputPassword = document.getElementById("password-input").value;
+    if (inputPassword && inputPassword.toLowerCase() === "flipforme") {
+      window.location.href =
+        "https://fod-client-staging-jlpc4.ondigitalocean.app/";
+    } else {
+      console.log("NO FLIP");
+    }
+  }
+
   return (
-    <div className="container">
+    <div className="container bg">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Flipped On Dogs</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <img
+          className="hero-image"
+          src="/FlippedOnDogsLogo.png"
+          alt="Flipped On Dogs Logo"
+        />
+        <p className="hero-text">
+          Enter your password
+          <br />
+          to play the game.
         </p>
+        <div className="flex-input">
+          <input id="password-input"></input>
+          <button className="submit-button" onClick={(e) => handleClick(e)}>
+            SUBMIT
+          </button>
+        </div>
+        <div className="footer">
+          <div className="social-text">
+            <p>
+              Don’t have a password yet?
+              <br />
+              Follow us for an invitation play Flipped on Dogs.
+            </p>
+          </div>
+          <a href="https://www.facebook.com/FlippedOnDogs/" target="_blank">
+            <img
+              className="share-icons"
+              src="/facebook-logo.png"
+              alt="Facebook logo"
+            />
+          </a>
+          <a href="https://www.instagram.com/flippedondogs" target="_blank">
+            <img
+              className="share-icons"
+              style={{ marginRight: "10px", marginLeft: "10px" }}
+              src="/instagram-logo.png"
+              alt="Instragram logo"
+            />
+          </a>
+          <a href="https://www.tiktok.com/@flippedondogs" target="_blank">
+            <img
+              className="share-icons"
+              src="/tiktok-logo.png"
+              alt="TikTok logo"
+            />
+          </a>
+        </div>
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
